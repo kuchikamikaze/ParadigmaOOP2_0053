4faff66e5845b99c24ae467fe54d9bc8b0b8c3f4
Naufal Arif <na713634@gmail.com>
2026-05-19 15:33:00 +0700

    class derivedclass cant use the fungtion from it's parrent

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -8,3 +8,16 @@ public:
     }
 };
 
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
