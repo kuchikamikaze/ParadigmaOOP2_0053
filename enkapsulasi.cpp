4f7d96605867870e464510ad72ea795be04a0cfd
Naufal Arif <na713634@gmail.com>
2026-05-19 15:40:56 +0700

    declaration class remotlampu and the member, there are private and public

---
+++ enkapsulasi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class remotelampu {
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
