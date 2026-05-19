efc1c959838c1a92b97d9d7722f1a7d03951d179
Naufal Arif <na713634@gmail.com>
2026-05-19 15:39:02 +0700

    class pelajar declraration and member that derived from class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -19,3 +19,10 @@ public:
     }
 };
 
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : 
+    orang(pUmur) {
+        cout << "pelajar dibuat\n " << umur << endl;
+    }
+};
